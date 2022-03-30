3b5a69446943e3d293f44283a56601f895f10e99
mabbason <miles.abbason@gmail.com>
2022-03-30 09:52:32 -0400

    modified gitignore and MongoDB

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
-const { response } = require('express')
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
+
 const app = express()
+
+
+// if (process.argv.length < 3) {
+//   console.log('Please provide the password as an argument: node mongo.js <password>')
+//   process.exit(1)
+// }
+
+
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true
-  }
-]
+
+
+
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     date: "2022-05-30T17:30:31.098Z",
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only Javascript",
+//     date: "2022-05-30T18:39:34.091Z",
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     date: "2022-05-30T19:20:14.298Z",
+//     important: true
+//   }
+// ]
 
 // app.get('/', (req, res) => {
 //   res.send('<h1>Hello World!</h1>')
 // })
+
+app.get('/api/notes', (req, res) => {
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
+})
 
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id)
@@ -42,10 +61,6 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(404).end()
   }
   res.json(note)
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -86,7 +101,7 @@ app.post('/api/notes', (req, res) => {
   res.json(note)
 })
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
